86bf5b401618e19067fe0ea15717a0cb01c8462c
Tobias Janke <tobias.janke@outlook.com>
2019-06-19 21:02:38 +0200

    Updated Version

--- PoliceBike/PoliceBike.ino
+++ PoliceBike/PoliceBike.ino
@@ -6,343 +6,269 @@
 #define BLINKYELLOW 40
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int dly = 150;
+unsigned int dly = 150;
 #define DEBOUNCE 50
-int previousState = LOW;
-int buttonState = 0;
-long lastDebounceTime = 0;
-unsigned long timePressed=0;
-int state = 0;
-int mode = 0;
+unsigned int previousState = LOW;
+unsigned int buttonState = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long timePressed = 0;
+unsigned int state = 0;
+unsigned int mode = 0;
 
-void reset(){
-  for(int s = 0; s < 24; s++){    
-    strip.setPixelColor(s,0, 0, 0);
-  }  
+void reset() {
+  for (unsigned int s = 0; s < 24; s++) {
+    strip.setPixelColor(s, 0, 0, 0);
+  }
   strip.show();
   noTone(4);
 }
 
-void switchL1(){
+void switchL1() {
   reset();
-  if(mode==0){
-    state = (state + 1)%4;      
-  }      
-  else if(mode==1){
-    state = (state + 1)%5; 
+  if (mode == 0) {
+    state = (state + 1) % 4;
+  }
+  else if (mode == 1) {
+    state = (state + 1) % 5;
   }
   else {
-    state = (state + 1)%3;        
+    state = (state + 1) % 3;
   }
 }
 
-void switchL2(){
+void switchL2() {
   reset();
-  mode=(mode + 1)%4;
-  state=0;
+  mode = (mode + 1) % 4;
+  state = 0;
 }
 
-void getButton(){
+void getButton() {
   unsigned long currentS = millis();
   buttonState = digitalRead(2);
- 
-  if ( (currentS - lastDebounceTime) > DEBOUNCE) {   
-    if(buttonState==HIGH && previousState==LOW){
+
+  if ( (currentS - lastDebounceTime) > DEBOUNCE) {
+    if (buttonState == HIGH && previousState == LOW) {
       timePressed = currentS;
       previousState = HIGH;
     }
-    else if(buttonState==LOW && previousState==HIGH){
+    else if (buttonState == LOW && previousState == HIGH) {
       unsigned long interval = currentS - timePressed;
       previousState = LOW;
-      timePressed=0;
-      if(interval > 1000){
+      timePressed = 0;
+      if (interval > 1000) {
         switchL2();
       }
       else {
         switchL1();
       }
     }
-   
-      lastDebounceTime = millis(); //set the current time 
+
+    lastDebounceTime = millis(); //set the current time
   }
 }
 
-int waitDly=500;
+unsigned int waitDly = 500;
 
-void wait(){
-  strip.setPixelColor(3-mode,0, BLINKYELLOW, 0);
+void wait() {
+  strip.setPixelColor(3 - mode, 0, BLINKYELLOW, 0);
   strip.show();
 }
 
-int index = 0;
-int leftSiren1[] =  {1,1,0,0,1,1,0,0};
-int rightSiren1[] = {0,0,1,1,0,0,1,1 };
-int leftSiren2[] =  {1,0,1,0,2,0,2,0};
-int rightSiren2[] = {2,0,2,0,1,0,1,0 };
+unsigned int index = 0;
+unsigned int leftSiren1[] =  {1, 1, 0, 0, 1, 1, 0, 0};
+unsigned int rightSiren1[] = {0, 0, 1, 1, 0, 0, 1, 1 };
+unsigned int leftSiren2[] =  {1, 0, 1, 0, 2, 0, 2, 0};
+unsigned int rightSiren2[] = {2, 0, 2, 0, 1, 0, 1, 0 };
 
-int horn1[]      = {440,585};
-int horn2[]      = {466,622};
+unsigned int horn1[]      = {440, 585};
+unsigned int horn2[]      = {466, 622};
 
-int dly1 = 150;
-int dly2 = 100;
+unsigned int dly1 = 150;
+unsigned int dly2 = 100;
 
-unsigned long previousMillis = 0; 
+unsigned long previousMillis = 0;
 
-int blinkDly=150;
-int blinkPos=1;
+unsigned int blinkDly = 150;
+unsigned int blinkPos = 1;
 
-void leftBlinker(){
-  int pixels[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-  for(int blk = 0; blk < blinkPos; blk++){
-    pixels[blk]=1;
-    pixels[7-blk]=1;
-    pixels[19-blk]=1;
-  }  
-
-  for(int s = 0; s < 24; s++){
-    if(pixels[s]==0){
-      strip.setPixelColor(s,0, 0, 0);
+void blink(unsigned int pixels[]) {
+  for (unsigned int s = 0; s < 24; s++) {
+    if (pixels[s] == 0) {
+      strip.setPixelColor(s, 0, 0, 0);
     }
     else {
-     strip.setPixelColor(s,BRIGHTNESS, BLINKYELLOW, 0);
-    }    
-  }  
-  
-  strip.show();
-  blinkPos = (blinkPos + 1)%5;
-}
-
-void rightBlinker(){
-  int pixels[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  
-  for(int blk = 0; blk < blinkPos; blk++){
-    pixels[8+blk]=1;
-    pixels[15-blk]=1;
-    pixels[20+blk]=1;    
-  }
-  
-  for(int s = 0; s < 24; s++){
-    if(pixels[s]==0){
-      strip.setPixelColor(s,0, 0, 0);
+      strip.setPixelColor(s, BRIGHTNESS, BLINKYELLOW, 0);
     }
-    else {
-     strip.setPixelColor(s,BRIGHTNESS, BLINKYELLOW, 0);
-    }    
   }
-  
   strip.show();
-  blinkPos = (blinkPos + 1)%5;
 }
 
-int warnIndex = 0;
+void leftBlinker() {
+  unsigned int pixels[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void warnBlinker(){
-  if(warnIndex==0 || warnIndex==2){
-    tone(4,1000);
+  for (unsigned int blk = 0; blk < blinkPos; blk++) {
+    pixels[blk] = 1;
+    pixels[7 - blk] = 1;
+    pixels[19 - blk] = 1;
+  }
+
+  blink(pixels);
+
+
+  blinkPos = (blinkPos + 1) % 5;
+}
+
+void rightBlinker() {
+  unsigned int pixels[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  for (unsigned int blk = 0; blk < blinkPos; blk++) {
+    pixels[8 + blk] = 1;
+    pixels[15 - blk] = 1;
+    pixels[20 + blk] = 1;
+  }
+
+  blink(pixels);
+
+  blinkPos = (blinkPos + 1) % 5;
+}
+
+unsigned int warnIndex = 0;
+
+void warnBlinker() {
+  if (warnIndex == 0 || warnIndex == 2) {
+    tone(4, 1000);
   }
   else {
     noTone(4);
   }
+  unsigned int r = (warnIndex % 2 == 0) ? BRIGHTNESS : 0;
+  unsigned int g = (warnIndex % 2 == 0) ? BLINKYELLOW : 0;
 
-  if(warnIndex%2==0){ 
-    for(int s = 0; s < 24; s++){
-      strip.setPixelColor(s,BRIGHTNESS, BLINKYELLOW, 0);    
-    }
+  for (unsigned int s = 0; s < 24; s++) {
+    strip.setPixelColor(s, r, g, 0);
   }
-  else {
-    for(int s = 0; s < 24; s++){
-      strip.setPixelColor(s,0, 0, 0);    
-    }
-  }
+
   strip.show();
-  warnIndex = (warnIndex + 1) %8;
+  warnIndex = (warnIndex + 1) % 8;
 }
 
 
-void siren(int leftS[], int rightS[]){
-  int redL = 0;
-  int redR = 0;
-  int blueL = 0;
-  int blueR = 0;  
-  
-  if(leftS[index]==0){
-      blueL = 0;
-      redL = 0;
-    }
-  else if(leftS[index]==1){
-    blueL = BRIGHTNESS;
-    redL = 0;
-  }
-  else if(leftS[index]==2) {
-    blueL = 0;
-    redL = BRIGHTNESS;
-  } 
+void siren(unsigned int leftS[], unsigned int rightS[]) {
+  unsigned int redL = (leftS[index] == 2) ? BRIGHTNESS : 0;
+  unsigned int redR = (rightS[index] == 2) ? BRIGHTNESS : 0;
+  unsigned int blueL = (leftS[index] == 1) ? BRIGHTNESS : 0;
+  unsigned int blueR = (rightS[index] == 1) ? BRIGHTNESS : 0;
 
-  if(rightS[index]==0){
-    blueR = 0;
-    redR = 0;
-  }
-  else if(rightS[index]==1){
-    blueR = BRIGHTNESS;
-    redR = 0;
-  }
-  else if(rightS[index]==2) {
-    blueR = 0;
-    redR = BRIGHTNESS;
-  }
-  
-  for(int i=0; i<24; i++){
-    if(i<8){
-      strip.setPixelColor(i,redL, 0, blueL);        
+
+  for (unsigned int i = 0; i < 24; i++) {
+    if (i < 8) {
+      strip.setPixelColor(i, redL, 0, blueL);
     }
-    else if(i<16){
-      strip.setPixelColor(i,redR, 0, blueR);  
+    else if (i < 16) {
+      strip.setPixelColor(i, redR, 0, blueR);
     }
-    else if(i < 20){
-      strip.setPixelColor(i,redL, 0, blueL);  
+    else if (i < 20) {
+      strip.setPixelColor(i, redL, 0, blueL);
     }
     else {
-      strip.setPixelColor(i,redR, 0, blueR);  
+      strip.setPixelColor(i, redR, 0, blueR);
     }
-    
-  }   
-  
+
+  }
+
   strip.show();
-  index = (index + 1)%8;
+  index = (index + 1) % 8;
 }
 
-void siren(int leftS[], int rightS[], int h[]){
-  int redL = 0;
-  int redR = 0;
-  int blueL = 0;
-  int blueR = 0;
-  
-  if(index == 0){
-    tone(4,h[0]);  
-  }
-  else if(index == 4){
-    tone(4,h[1]);  
-  }
-  
-  if(leftS[index]==0){
-      blueL = 0;
-      redL = 0;
-    }
-  else if(leftS[index]==1){
-    blueL = BRIGHTNESS;
-    redL = 0;
-  }
-  else if(leftS[index]==2) {
-    blueL = 0;
-    redL = BRIGHTNESS;
-  } 
+void siren(unsigned int leftS[], unsigned int rightS[], unsigned int h[]) {
 
-  if(rightS[index]==0){
-    blueR = 0;
-    redR = 0;
+  if (index == 0) {
+    tone(4, h[0]);
   }
-  else if(rightS[index]==1){
-    blueR = BRIGHTNESS;
-    redR = 0;
+  else if (index == 4) {
+    tone(4, h[1]);
   }
-  else if(rightS[index]==2) {
-    blueR = 0;
-    redR = BRIGHTNESS;
-  }
-  
-  for(int i=0; i<8; i++){
-    strip.setPixelColor(i,redL, 0, blueL);
-  }
-  for(int i=8; i<16; i++){
-    strip.setPixelColor(i,redR, 0, blueR);
-  } 
-  for(int i=16; i<20; i++){
-    strip.setPixelColor(i,redL, 0, blueL);
-  
-  for(int i=20; i<24; i++){
-    strip.setPixelColor(i,redR, 0, blueR);
-  }
-  
-  strip.show();
-  index = (index + 1)%8;  
+
+  siren(leftS, rightS);
 }
 
-int lightDly = 500;
+unsigned int lightDly = 500;
 
-void headLight(){
-  for(int i=0; i<16; i++){
-      strip.setPixelColor(i,BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
-    }
-  for(int i=16; i<24; i++){
-      strip.setPixelColor(i,BRIGHTNESS, 0, 0);
+void headLight() {
+  for (unsigned int i = 0; i < 16; i++) {
+    strip.setPixelColor(i, BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
+  }
+  for (unsigned int i = 16; i < 24; i++) {
+    strip.setPixelColor(i, BRIGHTNESS, 0, 0);
   }
   strip.show();
 }
 
-int strobeDly=30;
-int strobeOn = 0;
- 
-void strobeLight(){
-  if(strobeOn==0){
-    for(int i=0; i<24; i++){
-      strip.setPixelColor(i,0, 0, 0);
+unsigned int strobeDly = 30;
+unsigned int strobeOn = 0;
+
+void strobeLight() {
+
+  if (strobeOn == 0) {
+    for (unsigned int i = 0; i < 24; i++) {
+      strip.setPixelColor(i, 0, 0, 0);
     }
-    strobeOn=1;
+    strobeOn = 1;
   }
   else {
-    for(int i=0; i<24; i++){
-      strip.setPixelColor(i,BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
+    for (unsigned int i = 0; i < 24; i++) {
+      strip.setPixelColor(i, BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
     }
-    strobeOn=0;
+    strobeOn = 0;
   }
   strip.show();
 }
 
 
-int rbColors[8][3]={
-  {0,0,255},
-  {0,255,0},
-  {255,0,0},
-  {0,255,255},
-  {255,255,0},
-  {255,0,255},
-  {255,255,255},
-  {255,127,0}
+int rbColors[8][3] = {
+  {0, 0, 255},
+  {0, 255, 0},
+  {255, 0, 0},
+  {0, 255, 255},
+  {255, 255, 0},
+  {255, 0, 255},
+  {255, 255, 255},
+  {255, 127, 0}
 };
 
-int rbState[]={0,4,2,6,1,7,3,5,0,4,2,6,1,7,3,5,0,4,2,6,1,7,3,5};
-int rb1Dly=200;
+unsigned int rbState[] = {0, 4, 2, 6, 1, 7, 3, 5, 0, 4, 2, 6, 1, 7, 3, 5, 0, 4, 2, 6, 1, 7, 3, 5};
+unsigned int rb1Dly = 200;
 
-void rainbow1(){
-  for(int l = 0; l < 24; l++){
+void rainbow1() {
+  for (unsigned int l = 0; l < 24; l++) {
     strip.setPixelColor(l, rbColors[rbState[l]][0], rbColors[rbState[l]][1], rbColors[rbState[l]][2]);
-    rbState[l]=(rbState[l]+1)%8;
+    rbState[l] = (rbState[l] + 1) % 8;
   }
   strip.show();
 }
 
 
-int rb2Dly=200;
+unsigned int rb2Dly = 200;
 
-void rainbow2(){
-  for(int e = 0; e < 6; e++){
-    for(int l = e*4; l < (e+1)*4; l++){
-      strip.setPixelColor(l, rbColors[rbState[e]][0], rbColors[rbState[e]][1], rbColors[rbState[e]][2]);    
+void rainbow2() {
+  for (unsigned int e = 0; e < 6; e++) {
+    for (unsigned int l = e * 4; l < (e + 1) * 4; l++) {
+      strip.setPixelColor(l, rbColors[rbState[e]][0], rbColors[rbState[e]][1], rbColors[rbState[e]][2]);
     }
-    rbState[e]=(rbState[e]+1)%8;
+    rbState[e] = (rbState[e] + 1) % 8;
   }
-  
+
   strip.show();
 }
 
 
-void setup() {  
+void setup() {
   strip.begin();
   strip.show();
   pinMode(0, OUTPUT);
   pinMode(2, INPUT);
-  pinMode(4, OUTPUT); 
+  pinMode(4, OUTPUT);
 }
 
 
@@ -354,73 +280,84 @@ void loop() {
 
   if (currentMillis - previousMillis >= dly) {
     previousMillis = currentMillis;
-    if(mode==0){
-      if(state ==1){
-        dly = blinkDly;
-        leftBlinker();
-      }
-      else if(state == 2){
-        dly = blinkDly;
-        rightBlinker();
-      }
-      else if(state == 3){
-        dly = blinkDly;
-        warnBlinker();
-      }
-      else {
-        dly = waitDly;
-        wait();
-      }
+
+    switch (mode) {
+      case 0:
+        switch (state)
+        {
+          case 1:
+            dly = blinkDly;
+            leftBlinker();
+            break;
+          case 2:
+            dly = blinkDly;
+            rightBlinker();
+            break;
+          case 3:
+            dly = blinkDly;
+            warnBlinker();
+            break;
+          default:
+            dly = waitDly;
+            wait();
+        }
+        break;
+      case 1:
+        switch (state)
+        {
+          case 1:
+            dly = dly1;
+            siren(leftSiren1, rightSiren1);
+            break;
+          case 2:
+            dly = dly1;
+            siren(leftSiren1, rightSiren1, horn1);
+            break;
+          case 3:
+            dly = dly2;
+            siren(leftSiren2, rightSiren2);
+            break;
+          case 4:
+            dly = dly2;
+            siren(leftSiren2, rightSiren2, horn2);
+            break;
+          default:
+            dly = waitDly;
+            wait();
+        }
+        break;
+      case 2:
+        switch (state)
+        {
+          case 1:
+            dly = lightDly;
+            headLight();
+            break;
+          case 2:
+            dly = strobeDly;
+            strobeLight();
+            break;
+          default:
+            dly = waitDly;
+            wait();
+        }
+        break;
+      default:
+        switch (state)
+        {
+          case 1:
+            dly = rb1Dly;
+            rainbow1();
+            break;
+          case 2:
+            dly = rb2Dly;
+            rainbow2();
+            break;
+          default:
+            dly = waitDly;
+            wait();
+        }
     }
-    else if(mode==1) {
-      if(state==1){
-        dly = dly1;
-        siren(leftSiren1, rightSiren1);
-       }
-      else if(state==2){
-        dly = dly1;
-        siren(leftSiren1, rightSiren1, horn1);
-      }
-      else if(state==3){
-        dly = dly2;
-        siren(leftSiren2, rightSiren2);
-      }
-      else if(state==4){
-        dly = dly2;
-        siren(leftSiren2, rightSiren2, horn2);
-      }
-      else {
-         dly = waitDly;
-        wait();
-      }
-    } 
-    else if(mode==2) {
-      if(state==1){
-        dly=lightDly;
-        headLight();
-      }
-      else if(state==2){
-        dly=strobeDly;
-        strobeLight();
-      }
-      else {
-         dly = waitDly;
-          wait();
-      }
-    }
-    else {
-      if(state==1){
-        dly=rb1Dly;
-        rainbow1();
-      }
-      else if(state==2){
-        dly=rb2Dly;
-        rainbow2();
-      }
-      else {
-         dly = waitDly;
-          wait();
-      }
-    }
+
   }
 }
