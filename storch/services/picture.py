6c9592bd26d1dc42917a302b592255d833e0518c
tojanke <tobias.janke@outlook.com>
2021-02-13 14:25:13 +0100

    Better Performance

--- storch/services/picture.py
+++ storch/services/picture.py
@@ -8,35 +8,53 @@ from timeloop import Timeloop
 from datetime import timedelta
 
 nextPictureTime = 0
+nextConfUpdate = 0
 pictureCounter = 0
+pictureQuality = 50
+thumbFrequency = 15
+timelapseFrequency = 60
+configFrequency = 300
 
 mydb = mysql.connector.connect(host="localhost", user="service", password="storch",database='storch')
 mycursor = mydb.cursor()
 
-def takeImage():
-    global nextPictureTime
-    global pictureCounter
-    nextPictureTime = time.time()
-    pictureCounter = pictureCounter + 1
+def updateConfig():
+    global nextConfUpdate, pictureQuality, thumbFrequency, timelapseFrequency, configFrequency    
+    nextConfUpdate = time.time()
+
     mycursor.execute("SELECT confvalue FROM config WHERE confkey = 'picture-quality'")
     pictureQuality = int(mycursor.fetchone()[0])
+
     mycursor.execute("SELECT confvalue FROM config WHERE confkey = 'thumb-frequency'")
     thumbFrequency = int(mycursor.fetchone()[0])
 
+    mycursor.execute("SELECT confvalue FROM config WHERE confkey = 'timelapse-frequency'")
+    timelapseFrequency = int(mycursor.fetchone()[0])
+
+    mycursor.execute("SELECT confvalue FROM config WHERE confkey = 'picture-config-frequency'")
+    configFrequency = int(mycursor.fetchone()[0])
+
+    nextConfUpdate = nextConfUpdate + configFrequency
+
+
+
+def takeImage():
+    global nextPictureTime, pictureCounter, pictureQuality, thumbFrequency, timelapseFrequency   
+    nextPictureTime = time.time()
+    pictureCounter = pictureCounter + 1
+    
     picStream = io.BytesIO()
     with picamera.PiCamera() as camera:
-        camera.resolution = (3280, 2464)
-        camera.framerate = 20
+        camera.resolution = (1920, 1440)
+        camera.framerate = 10        
         camera.start_preview()
         time.sleep(10)    
         camera.capture(picStream, format='jpeg')
+    
     picStream.seek(0)
     originalImage = Image.open(picStream)
-
-    fullImage = originalImage.resize((1920,1440))
     fullStream = io.BytesIO()
-    fullImage.save(fullStream, format='jpeg',optimize=True,quality=pictureQuality)
-
+    originalImage.save(fullStream, format='jpeg',optimize=True,quality=pictureQuality)
     full_base64 = base64.encodebytes(fullStream.getvalue())
     fullStream.close()
     
@@ -60,12 +78,13 @@ def takeImage():
     
     picStream.close()    
 
-    mycursor.execute("SELECT confvalue FROM config WHERE confkey = 'timelapse-frequency'")
-    nextPictureTime = nextPictureTime + int(mycursor.fetchone()[0])
+    nextPictureTime = nextPictureTime + timelapseFrequency
     
 if __name__ == "__main__":
     while(True):
-        if (time.time() >= nextPictureTime):
+        timeNow = time.time()
+        if(timeNow >= nextConfUpdate):
+            updateConfig()
+        if (timeNow >= nextPictureTime):
             takeImage()
-            time.sleep(.5)
-    
+        time.sleep(1)
