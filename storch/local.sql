cc25287b159701a698ecabc8df90397fbda0f16e
tojanke <tobias.janke@outlook.com>
2021-02-13 13:22:50 +0100

    New Database Structure

--- storch/local.sql
+++ storch/local.sql
@@ -6,8 +6,17 @@ CREATE TABLE `config` (
 
 CREATE TABLE `pictures` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `thumb` blob NOT NULL,
   `full` mediumblob NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `thumbs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thumb` blob NOT NULL,
+  `pictureid` int(11) NOT NULL,
+  `online` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `thumb_to_pic` (`pictureid`),
+  CONSTRAINT `thumb_to_pic` FOREIGN KEY (`pictureid`) REFERENCES `pictures` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
